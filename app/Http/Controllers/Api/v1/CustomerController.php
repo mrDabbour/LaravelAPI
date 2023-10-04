182d7a2678ee131d0aa7686241e1a3508cc47206
mrDabbour <jmomj@gmail.com>
2023-10-04 22:40:38 +0200

    start work

---
+++ app/Http/Controllers/Api/v1/CustomerController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Resources\V1\CustomerResource;
+use App\Http\Resources\V1\CustomerCollection;
+use GuzzleHttp\Psr7\Request;
+
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+
+
+        $filter = new CustomerQuery();
+        $queryItem = $filter->transform($request);//[['column','operater','value']]
+
+
+        Customer::where(queryItem)
+
+
+        //
+        // return Customer::all();
+        // return new CustomerCollection(Customer::all());
+        return new CustomerCollection(Customer::paginate());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCustomerRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCustomerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Customer $customer)
+    {
+        //
+        // return $customer;
+        return new CustomerResource($customer); //there return just the id here ...
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCustomerRequest  $request
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCustomerRequest $request, Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Customer $customer)
+    {
+        //
+    }
+}
---
+++ app/Http/Resources/V1/InvoiceResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class InvoiceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        // return parent::toArray($request);
+
+
+        return [
+            'id' => $this->id,
+            'customerId' => $this->customer_id,
+            'amount' => $this->amount,
+            'status' => $this->status,
+            'billedDate' => $this->paid_date,
+
+        ];
+    }
+}
